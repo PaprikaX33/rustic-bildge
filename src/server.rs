cbfc526b64ef89e144581cc68f330d027fbcb913
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2025-01-03 06:25:48 +0800

    moved out the whole server logic from main

---
+++ src/server.rs
@@ -0,0 +1,49 @@
+use crate::configurator::AuthConfig;
+use axum::{extract::State, routing::get, Router};
+use std::sync::Arc;
+use tokio::runtime::Runtime;
+use tokio::sync::Notify;
+
+pub fn run_server(config: AuthConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let runtime = Runtime::new()?;
+    runtime.block_on(init_server(config));
+    Ok(())
+}
+
+async fn init_server(config: AuthConfig) {
+    let data = match config.drop_location.canonicalize() {
+        Ok(val) => val.display().to_string(),
+        Err(_) => "Invalid Path".to_string(),
+    };
+    let shutdown_trigger = Arc::new(Notify::new());
+    let app_state = AppState {
+        shutdown_trigger: shutdown_trigger.clone(),
+    };
+
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(move || async move { format!("{:?}", data) }))
+        .route("/kill", get(shutdown))
+        .with_state(app_state);
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", config.bind, config.port))
+        .await
+        .unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(async move { shutdown_trigger.notified().await })
+        .await
+        .expect("Failed to run server");
+}
+
+#[derive(Clone)]
+struct AppState {
+    shutdown_trigger: Arc<Notify>,
+}
+
+// Trigger shutdown handler
+async fn shutdown(State(state): State<AppState>) -> &'static str {
+    println!("Kill command received");
+    state.shutdown_trigger.notify_one();
+    "Shutting down..."
+}
