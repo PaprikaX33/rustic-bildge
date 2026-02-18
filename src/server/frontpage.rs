51e6802f47184b3bde3850e9e85cbe0dbf281097
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2026-02-18 18:35:29 +0800

    added favicon
    now the favicon returns the icon

--- src/server/frontpage.rs
+++ src/server/frontpage.rs
@@ -2,6 +2,9 @@ use axum::body::Body;
 use axum::http::StatusCode;
 use axum::response::Response;
 
+static FAVICON_DATA: &'static [u8] =
+    include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/ikon/icon.ico"));
+
 pub async fn serve() -> Response {
     Response::builder()
         .status(StatusCode::OK)
@@ -19,5 +22,12 @@ pub async fn invalid() -> Response {
             env!("CARGO_MANIFEST_DIR"),
             "/html/404.html"
         ))))
+        .unwrap()
+}
+
+pub async fn favicon() -> Response {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::from(FAVICON_DATA))
         .unwrap()
 }
