5df4edc3a07cd11f119e8a520ea7d4fe3aea2c46
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2026-03-24 18:04:41 +0800

    added the flag list
    it's runtime, and so terrible. Need to fully rework.
    Why is it so hard to do it in compile time in rust?????

--- src/server/util.rs
+++ src/server/util.rs
@@ -3,9 +3,21 @@ use axum::{
     response::Response,
     routing::{get, post},
 };
+
 #[cfg(feature = "util")]
 pub async fn version() -> Response {
     Response::builder()
         .body(Body::from(env!("CARGO_PKG_VERSION")))
+        .unwrap()
+}
+
+#[cfg(feature = "util")]
+pub async fn features() -> Response {
+    Response::builder()
+        .body(Body::from(format!(
+            "{} {}",
+            if cfg!(feature = "util") { "util" } else { "" },
+            if cfg!(feature = "tray") { "tray" } else { "" },
+        )))
         .unwrap()
 }
